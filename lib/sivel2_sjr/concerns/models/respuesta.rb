8c9b3f26febeee520fa27fe36bf38979bf9cddf1
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-02-25 08:14:30 -0500

    respuesta es modular

---
+++ lib/sivel2_sjr/concerns/models/respuesta.rb
@@ -0,0 +1,39 @@
+# encoding: UTF-8
+
+module Sivel2Sjr
+  module Concerns
+    module Models
+      module Respuesta
+        extend ActiveSupport::Concern
+
+        included do
+          belongs_to :caso, class_name: "Sivel2Sjr::Caso", 
+            foreign_key: "id_caso", validate: true
+
+          has_many :aslegal, class_name: "Sivel2Sjr::Aslegal", 
+            :through => :aslegal_respuesta
+          has_many :aslegal_respuesta, 
+            class_name: "Sivel2Sjr::AslegalRespuesta",  
+            foreign_key: "id_respuesta", dependent: :destroy, validate: true
+          accepts_nested_attributes_for :aslegal_respuesta, 
+            reject_if: :all_blank, update_only: true
+
+          has_many :ayudasjr, class_name: "Sivel2Sjr::Ayudasjr", 
+            :through => :ayudasjr_respuesta
+          has_many :ayudasjr_respuesta, 
+            class_name: "Sivel2Sjr::AyudasjrRespuesta",  
+            foreign_key: "id_respuesta", dependent: :destroy
+          # no funcionó accepts_nested_attributes_for :ayudasjr_respuesta, reject_if: :all_blank, update_only: true
+
+          has_many :derecho, class_name: "Sivel2Sjr::Derecho", 
+            :through => :derecho_respuesta
+          has_many :derecho_respuesta, 
+            class_name: "Sivel2Sjr::DerechoRespuesta",  
+            foreign_key: "id_respuesta", dependent: :destroy, validate: true
+          accepts_nested_attributes_for :derecho_respuesta, 
+            reject_if: :all_blank, update_only: true
+        end
+      end
+    end
+  end
+end
