1d827d8641a29fea3227bcbac1c62b9dfc30e509
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-03-17 03:47:10 -0500

    tipos de desplazamiento: rural-urbano/urbano-rural

---
+++ db/migrate/20150317084149_mastiposdesp.rb
@@ -0,0 +1,13 @@
+class Mastiposdesp < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+INSERT INTO sivel2_sjr_tipodesp (id, nombre, fechacreacion, created_at) VALUES (4, 'RURAL-URBANO', '2015-03-17', '2015-03-17');
+INSERT INTO sivel2_sjr_tipodesp (id, nombre, fechacreacion, created_at) VALUES (5, 'URBANO-RURAL', '2015-03-17', '2015-03-17');
+    SQL
+  end
+  def down
+    execute <<-SQL
+DELETE FROM sivel2_sjr_tipodesp WHERE id IN ('4', '5');
+    SQL
+  end
+end
