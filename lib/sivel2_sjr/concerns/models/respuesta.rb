083ad80f2851b1e3429ee36b759ba41cfd033251
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-03 09:47:30 -0500

    reorganiza

--- lib/sivel2_sjr/concerns/models/respuesta.rb
+++ lib/sivel2_sjr/concerns/models/respuesta.rb
@@ -10,28 +10,28 @@ module Sivel2Sjr
           belongs_to :caso, class_name: "Sivel2Gen::Caso", 
             foreign_key: "id_caso", validate: true
 
-          has_many :aslegal, class_name: "Sivel2Sjr::Aslegal", 
-            :through => :aslegal_respuesta
           has_many :aslegal_respuesta, 
             class_name: "Sivel2Sjr::AslegalRespuesta",  
             foreign_key: "id_respuesta", dependent: :destroy, validate: true
           accepts_nested_attributes_for :aslegal_respuesta, 
             reject_if: :all_blank, update_only: true
+          has_many :aslegal, class_name: "Sivel2Sjr::Aslegal", 
+            :through => :aslegal_respuesta
 
-          has_many :ayudasjr, class_name: "Sivel2Sjr::Ayudasjr", 
-            :through => :ayudasjr_respuesta
           has_many :ayudasjr_respuesta, 
             class_name: "Sivel2Sjr::AyudasjrRespuesta",  
             foreign_key: "id_respuesta", dependent: :destroy
           # no funcionó accepts_nested_attributes_for :ayudasjr_respuesta, reject_if: :all_blank, update_only: true
+          has_many :ayudasjr, class_name: "Sivel2Sjr::Ayudasjr", 
+            :through => :ayudasjr_respuesta
 
-          has_many :derecho, class_name: "Sivel2Sjr::Derecho", 
-            :through => :derecho_respuesta
           has_many :derecho_respuesta, 
             class_name: "Sivel2Sjr::DerechoRespuesta",  
             foreign_key: "id_respuesta", dependent: :destroy, validate: true
           accepts_nested_attributes_for :derecho_respuesta, 
             reject_if: :all_blank, update_only: true
+          has_many :derecho, class_name: "Sivel2Sjr::Derecho", 
+            :through => :derecho_respuesta
         end
       end
     end
