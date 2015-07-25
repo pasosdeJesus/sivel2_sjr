45386c4f6b975ff371380e46d6084e612269a15b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-25 09:16:24 -0500

    permite agregar mas de un acto

--- app/models/sivel2_sjr/actosjr.rb
+++ app/models/sivel2_sjr/actosjr.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 module Sivel2Sjr
   class Actosjr < ActiveRecord::Base
-    belongs_to :acto, class_name: "Sivel2Gen::Acto", validate: true,
+    belongs_to :acto, class_name: "Sivel2Gen::Acto", 
       foreign_key: "id_acto", inverse_of: :actosjr 
     belongs_to :desplazamiento, class_name: "Sivel2Sjr::Desplazamiento"
   end
