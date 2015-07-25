ae7753a90fb1fe35363e33e88b0887305fb78aa2
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-24 22:09:47 -0500

    Acreditaciones

---
+++ app/controllers/sivel2_sjr/admin/acreditaciones_controller.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+module Sivel2Sjr
+  module Admin
+    class AcreditacionesController < Sip::Admin::BasicasController
+      before_action :set_acreditacion, only: [:show, :edit, :update, :destroy]
+      load_and_authorize_resource class: Sivel2Sjr::Acreditacion
+
+      def clase 
+        "Sivel2Sjr::Acreditacion"
+      end
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_acreditacion
+        @basica = Sivel2Sjr::Acreditacion.find(params[:id])
+      end
+
+      def genclase
+        return 'M';
+      end
+
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def acreditacion_params
+        params.require(:acreditacion).permit(*atributos_form)
+      end
+
+    end
+  end
+end
