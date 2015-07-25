3f6933dc7327803d6bfae8055b19ea85e677daa3
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-07-24 20:11:57 -0500

    Semillas

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -4,7 +4,7 @@ conexion = ActiveRecord::Base.connection();
 
 # De motores
 Sip::carga_semillas_sql(conexion, 'sip', :datos)
-motor = ['sivel2_gen', '../..']
+motor = ['sivel2_gen', '../..', 'cor1440_gen']
 motor.each do |m|
     Sip::carga_semillas_sql(conexion, m, :cambios)
     Sip::carga_semillas_sql(conexion, m, :datos)
