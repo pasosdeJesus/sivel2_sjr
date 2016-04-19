d01f97575ce19f3820b8854e4f3507d8c514da1d
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2016-04-19 08:39:07 -0500

    personaliza cor1440

---
+++ app/controllers/cor1440_gen/actividades_controller.rb
@@ -0,0 +1,16 @@
+# encoding: UTF-8
+
+require_dependency 'cor1440_gen/concerns/controllers/actividades_controller.rb'
+
+class Cor1440Gen::ActividadesController < ApplicationController
+
+  # Funcion por sobrecargar para filtrar por otros parámetros personalizados
+  def self.filtramas(par, ac)
+    return ac
+  end
+
+  include Cor1440Gen::Concerns::Controllers::ActividadesController
+
+  Cor1440Gen.actividadg1 = "Mujeres empleadas SJR"
+  Cor1440Gen.actividadg3 = "Hombres empleados SJR"
+end
