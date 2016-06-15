823a575a3f9a5fca1de99df0f3a182be2eaa0135
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2016-06-14 22:28:11 -0500

    Arregla rutas

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -4,7 +4,17 @@ Rails.application.routes.draw do
 
   devise_scope :usuario do
     get 'sign_out' => 'devise/sessions#destroy'
+    
+    # El siguiente para superar mala generación del action en el formulario
+    # cuando se autentica mal (genera 
+    # /puntomontaje/puntomontaje/usuarios/sign_in )
+    if (Rails.configuration.relative_url_root != '/') 
+      ruta = File.join(Rails.configuration.relative_url_root, 
+                       'usuarios/sign_in')
+      post ruta, to: 'devise/sessions#create'
+    end
   end
+
   devise_for :usuarios, :skip => [:registrations], module: :devise
   as :usuario do
     get 'usuarios/edit' => 'devise/registrations#edit', 
@@ -21,11 +31,6 @@ Rails.application.routes.draw do
   get "/personas/remplazar" => 'sivel2_sjr/personas#remplazar'
 
   root 'sip/hogar#index'
-  mount Cor1440Gen::Engine, at: "/", as: "cor1440_gen"
-  mount Sivel2Sjr::Engine, at: "/", as: "sivel2_sjr"
-  mount Sivel2Gen::Engine, at: "/", as: "sivel2_gen"
-  mount Sip::Engine, at: "/", as: "sip"
-
   namespace :admin do
     Ability.tablasbasicas.each do |t|
       if (t[0] == "") 
@@ -35,4 +40,8 @@ Rails.application.routes.draw do
       end
     end
   end
+  mount Cor1440Gen::Engine, at: "/", as: "cor1440_gen"
+  mount Sivel2Sjr::Engine, at: "/", as: "sivel2_sjr"
+  mount Sivel2Gen::Engine, at: "/", as: "sivel2_gen"
+  mount Sip::Engine, at: "/", as: "sip"
 end
