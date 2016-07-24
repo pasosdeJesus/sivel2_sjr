4eb27fbb67baf7b9002cce9572566b3046b1f737
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-07-24 07:45:09 -0500

    tablas basicas

--- app/models/sivel2_sjr/ability.rb
+++ app/models/sivel2_sjr/ability.rb
@@ -4,24 +4,14 @@ module Sivel2Sjr
   class Ability < Sivel2Gen::Ability
     # Tablas básicas propias
     BASICAS_PROPIAS = [
-      ['Sivel2Sjr', 'acreditacion'],
       ['Sivel2Sjr', 'aslegal'], 
-      ['Sivel2Sjr', 'ayudaestado'], 
       ['Sivel2Sjr', 'ayudasjr'], 
-      ['Sivel2Sjr', 'clasifdesp'],
       ['Sivel2Sjr', 'comosupo'], 
-      ['Sivel2Sjr', 'declaroante'], 
       ['Sivel2Sjr', 'derecho'],
       ['Sivel2Sjr', 'idioma'],
-      ['Sivel2Sjr', 'inclusion'],
-      ['Sivel2Sjr', 'modalidadtierra'],
-      ['Sivel2Sjr', 'motivosjr'],
-      ['Sivel2Sjr', 'personadesea'], 
-      ['Sivel2Sjr', 'progestado'],
       ['Sivel2Sjr', 'proteccion'],
       ['Sivel2Sjr', 'rolfamilia'],
       ['Sivel2Sjr', 'statusmigratorio'],
-      ['Sivel2Sjr', 'tipodesp'],
     ]
     @@tablasbasicas = Sip::Ability::BASICAS_PROPIAS + 
       Cor1440Gen::Ability::BASICAS_PROPIAS +
