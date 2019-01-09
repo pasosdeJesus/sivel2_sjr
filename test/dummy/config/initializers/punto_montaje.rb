09e922bf745793ee1b699a16b0e3fd989479487b
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-01-09 18:16:29 -0500

    ruta sivel2sjr

---
+++ test/dummy/config/initializers/punto_montaje.rb
@@ -0,0 +1,3 @@
+Dummy::Application.config.relative_url_root = '/sivel2sjr'
+Dummy::Application.config.assets.prefix = '/sivel2sjr/assets/'
+
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,49 +2,52 @@
 
 Rails.application.routes.draw do
 
-  devise_scope :usuario do
-    get 'sign_out' => 'devise/sessions#destroy'
-    
-    # El siguiente para superar mala generación del action en el formulario
-    # cuando se autentica mal (genera 
-    # /puntomontaje/puntomontaje/usuarios/sign_in )
-    if (Rails.configuration.relative_url_root != '/') 
-      ruta = File.join(Rails.configuration.relative_url_root, 
-                       'usuarios/sign_in')
-      post ruta, to: 'devise/sessions#create'
-      get  ruta, to: 'devise/sessions#new'
+  scope 'sivel2sjr' do
+    devise_scope :usuario do
+      get 'sign_out' => 'devise/sessions#destroy'
+
+      # El siguiente para superar mala generación del action en el formulario
+      # cuando se autentica mal (genera 
+      # /puntomontaje/puntomontaje/usuarios/sign_in )
+      if (Rails.configuration.relative_url_root != '/') 
+        ruta = File.join(Rails.configuration.relative_url_root, 
+                         'usuarios/sign_in')
+        post ruta, to: 'devise/sessions#create'
+        get  ruta, to: 'devise/sessions#new'
+      end
     end
-  end
 
-  devise_for :usuarios, :skip => [:registrations], module: :devise
-  as :usuario do
-    get 'usuarios/edit' => 'devise/registrations#edit', 
-      :as => 'editar_registro_usuario'    
-    put 'usuarios/:id' => 'devise/registrations#update', 
-      :as => 'registro_usuario'            
-  end
-  resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' } 
+    devise_for :usuarios, :skip => [:registrations], module: :devise
+    as :usuario do
+      get 'usuarios/edit' => 'devise/registrations#edit', 
+        :as => 'editar_registro_usuario'    
+      put 'usuarios/:id' => 'devise/registrations#update', 
+        :as => 'registro_usuario'            
+    end
+    resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' } 
 
-  patch "/actos/agregar" => 'sivel2_sjr/actos#agregar'
-  get "/actos/eliminar" => 'sivel2_sjr/actos#eliminar'
-  
-  #get "/personas" => 'sivel2_sjr/personas#index'
-  #get "/personas/remplazar" => 'sivel2_sjr/personas#remplazar'
+    patch "/actos/agregar" => 'sivel2_sjr/actos#agregar'
+    get "/actos/eliminar" => 'sivel2_sjr/actos#eliminar'
 
-  root 'sip/hogar#index'
-  namespace :admin do
-    ab = ::Ability.new
-    ab.tablasbasicas.each do |t|
-      if (t[0] == "") 
-        c = t[1].pluralize
-        resources c.to_sym, 
-          path_names: { new: 'nueva', edit: 'edita' }
+    #get "/personas" => 'sivel2_sjr/personas#index'
+    #get "/personas/remplazar" => 'sivel2_sjr/personas#remplazar'
+
+    root 'sip/hogar#index'
+    namespace :admin do
+      ab = ::Ability.new
+      ab.tablasbasicas.each do |t|
+        if (t[0] == "") 
+          c = t[1].pluralize
+          resources c.to_sym, 
+            path_names: { new: 'nueva', edit: 'edita' }
+        end
       end
     end
   end
-  mount Cor1440Gen::Engine, at: "/", as: "cor1440_gen"
-  mount Heb412Gen::Engine, at: "/", as: "heb412_gen"
-  mount Sivel2Sjr::Engine, at: "/", as: "sivel2_sjr"
-  mount Sivel2Gen::Engine, at: "/", as: "sivel2_gen"
-  mount Sip::Engine, at: "/", as: "sip"
+
+  mount Cor1440Gen::Engine, at: "/sivel2sjr", as: "cor1440_gen"
+  mount Heb412Gen::Engine, at: "/sivel2sjr", as: "heb412_gen"
+  mount Sivel2Sjr::Engine, at: "/sivel2sjr", as: "sivel2_sjr"
+  mount Sivel2Gen::Engine, at: "/sivel2sjr", as: "sivel2_gen"
+  mount Sip::Engine, at: "/sivel2sjr", as: "sip"
 end
