b7848e9513c71e095c723401cf0274c434594451
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-08-06 19:49:29 -0500

    Arregla falla en actor social

--- app/models/sivel2_sjr/actosjr.rb
+++ app/models/sivel2_sjr/actosjr.rb
@@ -3,6 +3,7 @@ module Sivel2Sjr
   class Actosjr < ActiveRecord::Base
     belongs_to :acto, class_name: "Sivel2Gen::Acto", 
       foreign_key: "id_acto", inverse_of: :actosjr 
-    belongs_to :desplazamiento, class_name: "Sivel2Sjr::Desplazamiento"
+    belongs_to :desplazamiento, class_name: "Sivel2Sjr::Desplazamiento",
+      optional: true
   end
 end
