ec09fdf196e457857c9ace852b1dfa1b1651141f
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-09-24 06:22:08 -0500

    Actualiza

--- app/controllers/sivel2_sjr/casos_controller.rb
+++ app/controllers/sivel2_sjr/casos_controller.rb
@@ -3,7 +3,7 @@
 require_dependency 'sivel2_sjr/concerns/controllers/casos_controller'
 
 module Sivel2Sjr
-  class CasosController < ApplicationController
+  class CasosController < Heb412Gen::ModelosController
 
     include Sivel2Sjr::Concerns::Controllers::CasosController
 
