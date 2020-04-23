689c342410da4b937a1a6d38596587f39c0425a9
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-04-23 05:50:59 -0500

    Reorganiza medicion de indicadores usando marco generico de cor1440_gen

--- lib/sivel2_sjr/concerns/controllers/mindicadorespf_controller.rb
+++ lib/sivel2_sjr/concerns/controllers/mindicadorespf_controller.rb
@@ -44,57 +44,42 @@ module Sivel2Sjr
             [idscontactos, idsbenefindir]
           end
 
-          
-          def mideindicador_sivel2_sjr(mind, ind, fini, ffin)
-            resind = 0.0
-            idacs = []
-            urlevrind = ''
-            d1 = 0.0
-            urlev1 = ''
-            d2 = 0.0
-            urlev2 = ''
-            d3 = 0.0
-            urlev3 = ''
 
-            if ind.tipoindicador_id == 30 || ind.tipoindicador_id == 31
-              unicos = false
-              # Cuentas intermedias: (1) contactos + (2) beneficiarios 
-              if ind.tipoindicador_id == 31
-                # Cuentas intermedias: (1) contactos + (2) beneficiarios 
-                # unicos
-                unicos = true
-              end
-              idacs = calcula_listado_ac(mind.actividadpf_ids, fini, ffin)
-              hombrescasos = calcula_benef_por_sexo(idacs, 'M', ffin, unicos)
-              mujerescasos = calcula_benef_por_sexo(idacs, 'F', ffin, unicos)
-              sinsexocasos = calcula_benef_por_sexo(idacs, 'S', ffin, unicos)
-              benef_dir = hombrescasos[0] + mujerescasos[0] + sinsexocasos[0]
-              benef_indir = hombrescasos[1] + mujerescasos[1] + sinsexocasos[1]
-              resind = benef_dir.count + benef_indir.count
-              if idacs.count > 0
-                urlevrind = cor1440_gen.actividades_url +
-                  '?filtro[busid]=' + idacs.join(',')
-              end
-              if benef_dir.count > 0
-                urlevdir = sip.personas_path + '?filtro[busid]=' + 
-                  benef_dir.join(',')
-              end
-              if benef_indir.count > 0
-                urlevindir = sip.personas_path + '?filtro[busid]=' + 
-                  benef_indir.join(',')
-              end
-
-              return [ resind, urlevrind, 
-                       benef_dir.count, urlevdir, 
-                       benef_indir.count, urlevindir, 
-                       -1, '#' ]
+          # Auxiliar que resume medición de indicadores tipos 30 y 31          
+          def medir_indicador_personas_casos(idacs, mind, fini, ffin, unicos)
+            datosint = []
+            hombrescasos = calcula_benef_por_sexo(idacs, 'M', ffin, unicos)
+            mujerescasos = calcula_benef_por_sexo(idacs, 'F', ffin, unicos)
+            sinsexocasos = calcula_benef_por_sexo(idacs, 'S', ffin, unicos)
+            contactos = hombrescasos[0] + mujerescasos[0] + sinsexocasos[0]
+            familiares = hombrescasos[1] + mujerescasos[1] + sinsexocasos[1]
+            resind = contactos.count + familiares.count
+            datosint << { valor: contactos.count, rutaevidencia: '#' }
+            datosint << { valor: familiares.count, rutaevidencia: '#' }
+            if contactos.count > 0
+              datosint[0][:rutaevidencia] = sip.personas_path + 
+                '?filtro[busid]=' + contactos.join(',')
             end
-            return mideindicador_cor1440_gen(mind, ind, fini, ffin)
+            if familiares.count > 0
+              datosint[1][:rutaevidencia] = sip.personas_path + 
+                '?filtro[busid]=' + familiares.join(',')
+            end
+
+            return {resind: resind, datosint: datosint}
           end
 
+          # Medición de indicadores contando personas de casos en
+          # listados de casos (posiblemente repetidas)
+          def medir_indicador_res_tipo_30(idacs, mind, fini, ffin)
+            return medir_indicador_personas_casos(
+              idacs, mind, fini, ffin, false)
+          end
 
-          def mideindicador_particular(mind, ind, fini, ffin)
-            mideindicador_sivel2_sjr(mind, ind, fini, ffin)  
+          # Medición de indicadores contando personas únicas de casos en
+          # listados de casos 
+          def medir_indicador_res_tipo_31(idacs, mind, fini, ffin)
+            return medir_indicador_personas_casos(
+              idacs, mind, fini, ffin, true)
           end
 
         end #included
