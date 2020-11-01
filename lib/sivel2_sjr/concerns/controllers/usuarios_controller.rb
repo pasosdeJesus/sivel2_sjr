39f823331e1678ea34744a42320a1fef86542d75
alejocruzrcc <alejocruzzz@gmail.com>
2020-11-01 11:05:44 -0500

    crea lista params en usuarios

--- lib/sivel2_sjr/concerns/controllers/usuarios_controller.rb
+++ lib/sivel2_sjr/concerns/controllers/usuarios_controller.rb
@@ -45,20 +45,22 @@ module Sivel2Sjr
           end
 
           private
-
+          def lista_params
+            [
+             :id, :nusuario, :password, 
+             :nombre, :descripcion, :oficina_id,
+             :rol, :idioma, :email, :encrypted_password, 
+             :fechacreacion_localizada, :fechadeshabilitacion_localizada, 
+             :reset_password_token, 
+             :tema_id,
+             :reset_password_sent_at, :remember_created_at, :sign_in_count, 
+             :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, 
+             :failed_attempts, :unlock_token, :locked_at,
+             :last_sign_in_ip, :etiqueta_ids => []
+            ]
+          end
           def usuario_params
-            p = params.require(:usuario).permit(
-              :id, :nusuario, :password, 
-              :nombre, :descripcion, :oficina_id,
-              :rol, :idioma, :email, :encrypted_password, 
-              :fechacreacion_localizada, :fechadeshabilitacion_localizada, 
-              :reset_password_token, 
-              :tema_id,
-              :reset_password_sent_at, :remember_created_at, :sign_in_count, 
-              :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, 
-              :failed_attempts, :unlock_token, :locked_at,
-              :last_sign_in_ip, :etiqueta_ids => []
-            )
+            p = params.require(:usuario).permit(lista_params)
             return p
           end
 
