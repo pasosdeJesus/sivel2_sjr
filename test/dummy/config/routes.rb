a714e274e5aa8df2c72e28c083b916ef9db24c51
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-02-13 08:30:10 -0500

    Rutas

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,15 +2,16 @@
 
 Rails.application.routes.draw do
 
-  scope 'sivel2sjr' do
+  rutarel = ENV.fetch('RUTA_RELATIVA', 'sivel2sjr/')
+  scope rutarel do
     devise_scope :usuario do
       get 'sign_out' => 'devise/sessions#destroy'
 
       # El siguiente para superar mala generación del action en el formulario
-      # cuando se autentica mal (genera 
+      # cuando se autentica mal (genera
       # /puntomontaje/puntomontaje/usuarios/sign_in )
-      if (Rails.configuration.relative_url_root != '/') 
-        ruta = File.join(Rails.configuration.relative_url_root, 
+      if (Rails.configuration.relative_url_root != '/')
+        ruta = File.join(Rails.configuration.relative_url_root,
                          'usuarios/sign_in')
         post ruta, to: 'devise/sessions#create'
         get  ruta, to: 'devise/sessions#new'
@@ -19,36 +20,36 @@ Rails.application.routes.draw do
 
     devise_for :usuarios, :skip => [:registrations], module: :devise
     as :usuario do
-      get 'usuarios/edit' => 'devise/registrations#edit', 
-        :as => 'editar_registro_usuario'    
-      put 'usuarios/:id' => 'devise/registrations#update', 
-        :as => 'registro_usuario'            
+      get 'usuarios/edit' => 'devise/registrations#edit',
+        :as => 'editar_registro_usuario'
+      put 'usuarios/:id' => 'devise/registrations#update',
+        :as => 'registro_usuario'
     end
-    resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' } 
+    resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' }
 
-    patch "/actos/agregar" => 'sivel2_sjr/actos#agregar'
-    get "/actos/eliminar" => 'sivel2_sjr/actos#eliminar'
+    patch '/actos/agregar' => 'sivel2_sjr/actos#agregar'
+    get '/actos/eliminar' => 'sivel2_sjr/actos#eliminar'
 
-    #get "/personas" => 'sivel2_sjr/personas#index'
-    #get "/personas/remplazar" => 'sivel2_sjr/personas#remplazar'
+    #get '/personas' => 'sivel2_sjr/personas#index'
+    #get '/personas/remplazar' => 'sivel2_sjr/personas#remplazar'
 
     root 'sip/hogar#index'
     namespace :admin do
       ab = ::Ability.new
       ab.tablasbasicas.each do |t|
-        if (t[0] == "") 
+        if (t[0] == '')
           c = t[1].pluralize
-          resources c.to_sym, 
+          resources c.to_sym,
             path_names: { new: 'nueva', edit: 'edita' }
         end
       end
     end
   end
 
-  mount Sivel2Sjr::Engine, at: "/sivel2sjr", as: "sivel2_sjr"
-  mount Cor1440Gen::Engine, at: "/sivel2sjr", as: "cor1440_gen"
-  mount Sivel2Gen::Engine, at: "/sivel2sjr", as: "sivel2_gen"
-  mount Heb412Gen::Engine, at: "/sivel2sjr", as: "heb412_gen"
-  mount Mr519Gen::Engine, at: "/sivel2sjr", as: "mr519_gen"
-  mount Sip::Engine, at: "/sivel2sjr", as: "sip"
+  mount Sivel2Sjr::Engine, at: rutarel, as: 'sivel2_sjr'
+  mount Cor1440Gen::Engine, at: rutarel, as: 'cor1440_gen'
+  mount Sivel2Gen::Engine, at: rutarel, as: 'sivel2_gen'
+  mount Heb412Gen::Engine, at: rutarel, as: 'heb412_gen'
+  mount Mr519Gen::Engine, at: rutarel, as: 'mr519_gen'
+  mount Sip::Engine, at: rutarel, as: 'sip'
 end
