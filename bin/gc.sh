bce37dddc490243308f6774e5140e30f3747693b
Nombre Usuario <correo@usuario.net>
2022-02-18 12:17:14 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-# Hace pruebas, pruebas de regresión y envia a github
+# Actualiza dependencias, revisa errores comunes, 
+# ejecuta pruebas y envía a repositorio
 
+if (test -f ".env") then {
+  rutaap="./"
+} elif (test -f "test/dummy/.env") then {
+  rutaap="test/dummy"
+} else {
+  echo "No se determino ruta de aplicación. Falta archivo .env"
+  exit 1;
+} fi;
+
+echo "Ruta de la aplicación: $rutaap"
 
 s=`grep -B 1 "^ *path" Gemfile 2> /dev/null`
 if (test "$?" = "0") then {
@@ -8,12 +19,12 @@ if (test "$?" = "0") then {
   exit 1;
 } fi;
 
-grep "^ *gem.*debugger" Gemfile > /dev/null 2> /dev/null
+grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
-	echo "Gemfile incluye debugger que heroku no quiere"
+	echo "Gemfile incluye debugger"
 	exit 1;
 } fi;
-grep "^ *gem.*byebug" Gemfile > /dev/null 2> /dev/null
+grep "^ *gem *.byebug*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
 	echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
 	exit 1;
@@ -24,49 +35,50 @@ if (test "$SINAC" != "1") then {
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
-	(cd test/dummy; CXX=c++ yarn upgrade)
+	(cd $rutaap; CXX=c++ yarn upgrade)
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
+
 if (test "$SININS" != "1") then {
 	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle install
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
-	(cd test/dummy; CXX=c++ yarn install)
+	(cd $rutaap; CXX=c++ yarn install; bin/rails assets:precompile)
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
-
 
 if (test "$SINMIG" != "1") then {
-	(cd test/dummy; bin/rails db:migrate sip:indices db:schema:dump)
+	(cd $rutaap; bin/rails db:migrate sip:indices db:schema:dump)
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
 
-(cd test/dummy; RAILS_ENV=test bundle exec rake db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices)
+(cd $rutaap; RAILS_ENV=test bin/rails db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices)
 if (test "$?" != "0") then {
 	echo "No puede preparse base de prueba";
 	exit 1;
 } fi;
 
-bin/rails test
+CONFIG_HOSTS=www.example.com bin/rails test
 if (test "$?" != "0") then {
-	echo "No pasaron pruebas";
+	echo "No pasaron pruebas de regresion";
 	exit 1;
 } fi;
 
-(cd test/dummy; CONFIG_HOSTS=127.0.0.1 bin/rails test:system)
+(cd $rutaap; CONFIG_HOSTS=127.0.0.1 bin/rails test:system)
 if (test "$?" != "0") then {
 	echo "No pasaron pruebas del sistema";
 	exit 1;
 } fi;
 
-(cd test/dummy; RAILS_ENV=test bundle exec rake db:schema:dump)
+
+(cd $rutaap; RAILS_ENV=test bin/rails db:schema:dump)
 
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
 git status -s
@@ -79,3 +91,4 @@ if (test "$?" != "0") then {
 	echo "No pudo subirse el cambio a github";
 	exit 1;
 } fi;
+
