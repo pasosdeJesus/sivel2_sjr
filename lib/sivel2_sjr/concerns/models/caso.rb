e6de0e92daf4c7d3ff099d3f3e21ff7f1d3e4612
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-03-29 06:17:41 -0500

    Refactoriza en si_jrscol

--- lib/sivel2_sjr/concerns/models/caso.rb
+++ lib/sivel2_sjr/concerns/models/caso.rb
@@ -8,43 +8,38 @@ module Sivel2Sjr
         include Sivel2Gen::Concerns::Models::Caso
 
         included do
-          has_many :actosjr, class_name: 'Sivel2Sjr::Actosjr',
-            :through => :acto, dependent: :destroy
-          accepts_nested_attributes_for :actosjr, allow_destroy: true, 
-            reject_if: :all_blank
-
-          has_one :casosjr, class_name: 'Sivel2Sjr::Casosjr',
-            foreign_key: "id_caso", inverse_of: :caso, validate: true, 
-            dependent: :destroy
-          accepts_nested_attributes_for :casosjr, allow_destroy: true, 
-            update_only: true
-
-          # respuesta deberìa ser con :through => :casosjr pero más dificil guardar
-          has_many :respuesta, class_name: 'Sivel2Sjr::Respuesta',
-            foreign_key: "id_caso", validate:true, dependent: :destroy
-          accepts_nested_attributes_for :respuesta, allow_destroy: true, 
-            reject_if: :all_blank
-
-          has_many :desplazamiento, class_name: 'Sivel2Sjr::Desplazamiento',
-            foreign_key: "id_caso", validate: true, dependent: :destroy
-          accepts_nested_attributes_for :desplazamiento , allow_destroy: true, 
-            reject_if: :all_blank
-          has_many :victimasjr, class_name: 'Sivel2Sjr::Victimasjr',
-            :through => :victima, dependent: :destroy
-          accepts_nested_attributes_for :victimasjr, allow_destroy: true, 
-            reject_if: :all_blank
-
+#          has_many :actosjr, class_name: 'Sivel2Sjr::Actosjr',
+#            :through => :acto, dependent: :destroy
+#          accepts_nested_attributes_for :actosjr, allow_destroy: true, 
+#            reject_if: :all_blank
+#
+#          has_one :casosjr, class_name: 'Sivel2Sjr::Casosjr',
+#            foreign_key: "id_caso", inverse_of: :caso, validate: true, 
+#            dependent: :destroy
+#          accepts_nested_attributes_for :casosjr, allow_destroy: true, 
+#            update_only: true
+#
+#          # respuesta deberìa ser con :through => :casosjr pero más dificil guardar
+#          has_many :respuesta, class_name: 'Sivel2Sjr::Respuesta',
+#            foreign_key: "id_caso", validate:true, dependent: :destroy
+#          accepts_nested_attributes_for :respuesta, allow_destroy: true, 
+#            reject_if: :all_blank
+#
+#          has_many :desplazamiento, class_name: 'Sivel2Sjr::Desplazamiento',
+#            foreign_key: "id_caso", validate: true, dependent: :destroy
+#          accepts_nested_attributes_for :desplazamiento , allow_destroy: true, 
+#            reject_if: :all_blank
+#          has_many :victimasjr, class_name: 'Sivel2Sjr::Victimasjr',
+#            :through => :victima, dependent: :destroy
+#          accepts_nested_attributes_for :victimasjr, allow_destroy: true, 
+#            reject_if: :all_blank
+#
         end
 
 
 
         module ClassMethods
 
-        end
-
-
-        def iporsivel2sjr
-          "por"
         end
 
       end
